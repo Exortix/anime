3a2bc110ebff8dbb49ea6a7db198c54f4be521bc
Exortix <40566204+Exortix@users.noreply.github.com>
2021-02-07 04:34:30 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,85 +1,159 @@
 var myArray = []
 let url = 'https://api.jikan.moe/v3'
 
+$( document ).ready(function() {
+    switch ($('#filter').val()) {
+        case '1':
+            window.document.title='Anime'
+            $('#title').html(`My<strong>Anime</strong>ListSearch`)
+            $.ajax({
+                method:'GET',
+                url:`${url}/top/anime`,
+                success:function (response) {
+                    myArray = response.top
+                },
+                complete:function () {
+                    buildAnime(myArray)
+                }
+            }) 
+            break;
+        case '2':
+            window.document.title='Manga'
+            $('#title').html(`My<strong>Manga</strong>ListSearch`)
+            window.document.title='Anime'
+            $('#title').html(`My<strong>Anime</strong>ListSearch`)
+            $.ajax({
+                method:'GET',
+                url:`${url}/top/manga`,
+                success:function (response) {
+                    myArray = response.top
+                },
+                complete:function () {
+                    buildAnime(myArray)
+                }
+            }) 
+            break;
+    }
+});
+
 $('#filter').change( () => {
     switch ($('#filter').val()) {
         case '1':
             window.document.title='Anime'
             $('#title').html(`My<strong>Anime</strong>ListSearch`)
+            $.ajax({
+                method:'GET',
+                url:`${url}/top/anime`,
+                success:function (response) {
+                    myArray = response.top
+                },
+                complete:function () {
+                    buildAnime(myArray)
+                }
+            }) 
             break;
         case '2':
             window.document.title='Manga'
             $('#title').html(`My<strong>Manga</strong>ListSearch`)
+            $.ajax({
+                method:'GET',
+                url:`${url}/top/manga`,
+                success:function (response) {
+                    myArray = response.top
+                },
+                complete:function () {
+                    buildAnime(myArray)
+                }
+            })
             break;
     }
 });
 
 function search(query) {
-    switch ($('#filter').val()) {
-        case '1':
-            $.ajax({
-                method:'GET',
-                url:`${url}/search/anime?q=${query}`,
-                success:function (response) {
-                    myArray = response
-                    buildAnime(myArray)
-                }
-            })           
-            break;
-        case '2':
-            $.ajax({
-                method:'GET',
-                url:`${url}/search/manga?q=${query}&page=1`,
-                success:function (response) {
-                    myArray = response.results
-                    $.ajax({
-                        method:'GET',
-                        url:`${url}/search/manga?q=${query}&page=1&genre=9`,
-                        success:function (response) {
-                            myArray = _(myArray) 
-                            .differenceBy(response.results, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date')
-                            .map(_.partial(_.pick, _, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date'))
-                            .value();
-                            $.ajax({
-                                method:'GET',
-                                url:`${url}/search/manga?q=${query}&page=1&genre=12`,
-                                success:function (response) {
-                                    myArray = _(myArray) 
-                                    .differenceBy(response.results, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date')
-                                    .map(_.partial(_.pick, _, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date'))
-                                    .value();
-                                    $.ajax({
-                                        method:'GET',
-                                        url:`${url}/search/manga?q=${query}&page=1&genre=33`,
-                                        success:function (response) {
-                                            myArray = _(myArray) 
-                                            .differenceBy(response.results, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date')
-                                            .map(_.partial(_.pick, _, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date'))
-                                            .value();
-                                            $.ajax({
-                                                method:'GET',
-                                                url:`${url}/search/manga?q=${query}&page=1&genre=34`,
-                                                success:function (response) {
-                                                    myArray = _(myArray) 
-                                                    .differenceBy(response.results, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date')
-                                                    .map(_.partial(_.pick, _, 'mal_id', 'url', 'image_url', 'synopsis', 'type', 'volumes', 'title', 'score', 'start_date'))
-                                                    .value();
-                                                },
-                                                complete:function () {
-                                                    buildManga(myArray)
-                                                }
-                                            })
-                                        }
-                                    })
-                                }
-                            })
-                        }
-                    }) 
-                }
-            }) 
-            break;
-        default:
-            break;
+    if ($('#search').val().length < 3) {
+        switch ($('#filter').val()) {
+            case '1':
+                $.ajax({
+                    method:'GET',
+                    url:`${url}/top/anime`,
+                    success:function (response) {
+                        myArray = response.top
+                    },
+                    complete:function () {
+                        buildAnime(myArray)
+                    }
+                }) 
+                break;
+            case '2':
+                $.ajax({
+                    method:'GET',
+                    url:`${url}/top/manga`,
+                    success:function (response) {
+                        myArray = response.top
+                    },
+                    complete:function () {
+                        buildAnime(myArray)
+                    }
+                })
+                break;
+        }
+    } else {
+        switch ($('#filter').val()) {
+            case '1':
+                $.ajax({
+                    method:'GET',
+                    url:`${url}/search/anime?q=${query}`,
+                    success:function (response) {
+                        myArray = response.results
+                    },
+                    complete:function () {
+                        buildAnime(myArray)
+                    }
+                })           
+                break;
+            case '2':
+                $.ajax({
+                    method:'GET',
+                    url:`${url}/search/manga?q=${query}&page=1`,
+                    success:function (response) {
+                        myArray = response.results
+                        $.ajax({
+                            method:'GET',
+                            url:`${url}/search/manga?q=${query}&page=1&genre=12`,
+                            success:function (response) {
+                                myArray = _(myArray) 
+                                .differenceBy(response.results, 'mal_id')
+                                .value();
+                                console.log(myArray)
+                                $.ajax({
+                                    method:'GET',
+                                    url:`${url}/search/manga?q=${query}&page=1&genre=33`,
+                                    success:function (response) {
+                                        myArray = _(myArray) 
+                                        .differenceBy(response.results, 'mal_id')
+                                        .value();
+                                        $.ajax({
+                                            method:'GET',
+                                            url:`${url}/search/manga?q=${query}&page=1&genre=34`,
+                                            success:function (response) {
+                                                myArray = _(myArray) 
+                                                .differenceBy(response.results, 'mal_id')
+                                                .value();
+                                            },
+                                            complete:function () {
+                                                buildManga(myArray)
+                                            }
+                                        })
+                                    }
+                                })
+                            }
+                        }) 
+                    }   
+                }) 
+                break;
+            default:
+                break;
+        }
     }
 }
 
@@ -87,26 +161,27 @@ function buildAnime(data){
     var animeList = document.getElementById('myAnimeList')
     animeList.querySelector('.row').innerHTML = ''
 
-    for (var i = 0; i < Object.keys(data.results).length; i++){
-        if (!(data.results[i].rated == 'Rx'||  data.results[i].rated == 'R+')) {
+    for (var i = 0; i < Object.keys(data).length; i++){
+        if (!(data[i].rated == 'Rx'||  data[i].rated == 'R+')) {
 
             var anime = `
                             <div class="col anime rounded">
-                                    <a href=${data.results[i].url} target="_blank"><img class='rounded' src =${data.results[i].image_url}/></a>
-                                    <h4>${data.results[i].title}</h4>
+                                    <a href=${data[i].url} target="_blank"><img class='rounded' src =${data[i].image_url}/></a>
+                                    <h4>${data[i].title}</h4>
                                     <div class="row rounded" id='anime-stats'>
                                         <div class="col">
-                                        ${data.results[i].type} (${data.results[i].episodes} eps)
+                                        ${data[i].type} (${data[i].episodes} eps)
                                         </div>
                                         <div class="col">
-                                        ${(data.results[i].start_date != null) ? data.results[i].start_date.split('').slice(0, 4).join("") : ''}
+                                        ${(data[i].start_date != null) ? data[i].start_date.split('').slice(0, 4).join("") : ''}
                                         </div>
                                         <div class="col">
-                                        ${data.results[i].score}
+                                        ${data[i].score}
                                         </div>
                                     </div>
-                                    <p>${data.results[i].synopsis}</p>
-                                    <a href=${data.results[i].url} target="_blank" class="btn btn-primary">read more</a>
+                                    <p>${data[i].synopsis}</p>
+                                    <a href=${data[i].url} target="_blank" class="btn btn-secondary">Details</a>
+                                    <a href='https://www12.9anime.to/search?keyword=${data[i].title}' target="_blank" class="btn btn-primary">Watch</a>
                             </div>
                         `
             animeList.querySelector('.row').innerHTML += anime
@@ -136,13 +211,18 @@ function buildManga(data){
                                         </div>
                                     </div>
                                     <p>${data[i].synopsis}</p>
-                                    <a href=${data[i].url} target="_blank" class="btn btn-primary">read more</a>
+                                    <a href=${data[i].url} target="_blank" class="btn btn-secondary">Details</a>
+                                    <a href='https://mangakakalot.com/search/story/${data[i].title.replace(/[\W_]/g, " ").trim().split(' ').join('_')}' target="_blank" class="btn btn-primary">Read</a>
+                                    
                             </div>
                         `
             mangaList.querySelector('.row').innerHTML += manga
         }
     }
 }
+
+
+
 
 
 // Execute a function when the user releases a key on the keyboard
